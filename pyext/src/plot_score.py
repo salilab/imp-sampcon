8e2930586fc59830ca2d13725428ae24432962ac
shruthi <shruthi.viswanath@gmail.com>
2018-05-22 14:49:52 -0700

    added more metrics for choosing GSMs

--- pyext/src/plot_score.py
+++ pyext/src/plot_score.py
@@ -7,6 +7,11 @@ score_file=sys.argv[1]
 score_name=sys.argv[2]
 
 scores=numpy.loadtxt(score_file)
+print "Mean",score_name,numpy.mean(scores)
+print "Standard deviation",score_name,numpy.std(scores)
+
+print "Mean minus std_dev", score_name, numpy.mean(scores) - numpy.std(scores)
+print "Mean minus half std_dev", score_name, numpy.mean(scores) - 0.5*numpy.std(scores)
 
 plt.hist(scores,bins=100)
 plt.xlabel(score_name)
