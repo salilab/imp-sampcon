756e9e7ae2f523f468559a30372f9d57f60bb854
Ben Webb <ben@salilab.org>
2020-01-14 10:30:23 -0800

    Clean up "all scores" plot
    - Share logic with one-column plotting
    - Exclude non-scores (e.g. model index) from plotting
    - Add test.

--- pyext/src/plot_score.py
+++ pyext/src/plot_score.py
@@ -4,26 +4,10 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import os,sys,string,pandas
 
-score_file=sys.argv[1]
-column=sys.argv[2]
 
-scores=pandas.read_csv(score_file, sep=' ')
-show = (len(sys.argv) >= 4 and sys.argv[3] == "True")
-
-if column=="all":
-    print("Plotting all columns")
-    for k in scores.columns:
-        print(k, "|| Mean:", scores[k].mean(), "| SD:", scores[k].std())
-        plt.hist(scores[k],bins=100)
-        plt.xlabel(k)
-        plt.ylabel('P')
-        plt.title(k)
-
-        plt.savefig(k+'.jpg',dpi=300)
-        plt.clf()
-elif column in list(scores.columns.values):
-
-    print(column, "|| Mean:", scores[column].mean(), "| SD:", scores[column].std())
+def plot_column(scores, column, show):
+    print(column, "|| Mean:", scores[column].mean(),
+          "| SD:", scores[column].std())
     plt.hist(scores[column], bins=100)
     plt.xlabel(column)
     plt.ylabel('P')
@@ -32,6 +16,24 @@ elif column in list(scores.columns.values):
         plt.show()
     else:
         plt.savefig(column+".png", dpi=300)
+    return plt
+
+
+score_file=sys.argv[1]
+column=sys.argv[2]
+
+scores=pandas.read_csv(score_file, sep=' ')
+show = (len(sys.argv) >= 4 and sys.argv[3] == "True")
+
+if column=="all":
+    print("Plotting all columns")
+    exclude = frozenset(('Model_index', 'Replica_id', 'Frame_id', 'Run_id'))
+    for k in scores.columns:
+        if k not in exclude:
+            plt = plot_column(scores, k, show=False)
+            plt.clf()
+elif column in scores.columns.values:
+    plot_column(scores, column, show=show)
 else:
     print(column, "is not a valid score parameter. Use one of:")
     print(scores.columns.values)
--- test/test_plot_score.py
+++ test/test_plot_score.py
@@ -10,8 +10,8 @@ TOPDIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 
 class Tests(unittest.TestCase):
-    def test_plot_score(self):
-        """Test plot_score.py"""
+    def test_plot_score_one(self):
+        """Test plot_score.py with one score"""
         script = utils.get_script(TOPDIR, 'plot_score.py')
         inp_dir = os.path.join(TESTDIR, 'input')
         subprocess.check_call(
@@ -19,6 +19,19 @@ class Tests(unittest.TestCase):
              os.path.join(inp_dir, 'model_ids_scores.txt'),
              'CrossLinkingMassSpectrometryRestraint_Data_Score_Chen'])
         os.unlink('CrossLinkingMassSpectrometryRestraint_Data_Score_Chen.png')
+
+    def test_plot_score_all(self):
+        """Test plot_score.py with all scores"""
+        script = utils.get_script(TOPDIR, 'plot_score.py')
+        inp_dir = os.path.join(TESTDIR, 'input')
+        subprocess.check_call(
+            [sys.executable, script,
+             os.path.join(inp_dir, 'model_ids_scores.txt'), 'all'])
+        os.unlink('ConnectivityRestraint_Rpb1.png')
+        os.unlink('CrossLinkingMassSpectrometryRestraint_Distance_.png')
+        os.unlink('CrossLinkingMassSpectrometryRestraint_Data_Score_Chen.png')
+        os.unlink('Total_Score.png')
+        os.unlink('ExcludedVolumeSphere_None.png')
 
 
 if __name__ == '__main__':
