c9c00b6a07656afab165144d6d4b71fc027e06cc
Ben Webb <ben@salilab.org>
2020-01-15 14:20:36 -0800

    Test filtering with only one run

--- test/test_select_good_scoring.py
+++ test/test_select_good_scoring.py
@@ -40,6 +40,38 @@ class Tests(unittest.TestCase):
             os.rmdir(os.path.join(mod_dir, 'good_scoring_models'))
             os.rmdir(os.path.join(mod_dir, 'filter'))
 
+    def test_select_good_scoring_models_one_run(self):
+        """Test select_good_scoring_models.py with only one run"""
+        script = utils.get_script(TOPDIR, 'select_good_scoring_models.py')
+        with utils.temporary_directory() as tmpdir:
+            mod_dir = os.path.join(tmpdir, 'modeling')
+            shutil.copytree(os.path.join(TESTDIR, 'modeling'), mod_dir)
+            # Keep only run1
+            shutil.rmtree(os.path.join(mod_dir, 'run2'))
+            subprocess.check_call(
+                [sys.executable, script,
+                 '-rd', mod_dir, '-rp', 'run',
+                 '-sl', 'CrossLinkingMassSpectrometryRestraint_Distance_',
+                 '-pl', 'ConnectivityRestraint_Rpb1',
+                 'CrossLinkingMassSpectrometryRestraint_Data_Score_Chen',
+                 'ExcludedVolumeSphere_None', 'Total_Score',
+                 '-alt', '1.0', '-aut', '1.0', '-mlt', '0.0', '-mut', '30.0'])
+            # With only a single run, everything goes into sample A
+            for score, exp_wc in (('A', 48), ('B', 0)):
+                score_file = os.path.join(mod_dir, 'good_scoring_models',
+                                          'scores%s.txt' % score)
+                with open(score_file) as fh:
+                    wc = len(fh.readlines())
+                self.assertEqual(wc, exp_wc)
+                os.unlink(score_file)
+            model_ids = os.path.join(mod_dir, 'filter', 'model_ids_scores.txt')
+            with open(model_ids) as fh:
+                wc = len(fh.readlines())
+            self.assertEqual(wc, 49)
+            os.unlink(model_ids)
+            os.rmdir(os.path.join(mod_dir, 'good_scoring_models'))
+            os.rmdir(os.path.join(mod_dir, 'filter'))
+
     def test_select_good_scoring_models_extract(self):
         """Test select_good_scoring_models.py with extract"""
         script = utils.get_script(TOPDIR, 'select_good_scoring_models.py')
