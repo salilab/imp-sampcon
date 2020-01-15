b86c842eba4d69c650334e664248f78269e340a3
Ben Webb <ben@salilab.org>
2020-01-15 14:23:36 -0800

    Test extract with only one run

--- test/test_select_good_scoring.py
+++ test/test_select_good_scoring.py
@@ -114,6 +114,50 @@ class Tests(unittest.TestCase):
 
             os.rmdir(gsm_dir)
 
+    def test_select_good_scoring_models_extract_one_run(self):
+        """Test select_good_scoring_models.py with extract, one run"""
+        script = utils.get_script(TOPDIR, 'select_good_scoring_models.py')
+        with utils.temporary_directory() as tmpdir:
+            mod_dir = os.path.join(tmpdir, 'modeling')
+            shutil.copytree(os.path.join(TESTDIR, 'modeling'), mod_dir)
+            # Keep only run1
+            shutil.rmtree(os.path.join(mod_dir, 'run2'))
+            subprocess.check_call(
+                [sys.executable, script,
+                 '-rd', mod_dir, '-rp', 'run',
+                 '-sl', 'CrossLinkingMassSpectrometryRestraint_Distance_',
+                 '-pl', 'ConnectivityRestraint_Rpb1',
+                 'CrossLinkingMassSpectrometryRestraint_Data_Score_Chen',
+                 'ExcludedVolumeSphere_None', 'Total_Score',
+                 '-alt', '1.0', '-aut', '1.0', '-mlt', '0.0', '-mut', '12.0',
+                 '-e'])
+            gsm_dir = os.path.join(mod_dir, 'good_scoring_models')
+            for score, num in (('A', 2), ('B', 3)):
+                score_file = os.path.join(gsm_dir, 'scores%s.txt' % score)
+                with open(score_file) as fh:
+                    wc = len(fh.readlines())
+                self.assertEqual(wc, num)
+                os.unlink(score_file)
+            model_ids = os.path.join(gsm_dir, 'model_ids_scores.txt')
+            with open(model_ids) as fh:
+                wc = len(fh.readlines())
+            self.assertEqual(wc, 6)
+            os.unlink(model_ids)
+
+            model_ids = os.path.join(gsm_dir, 'model_sample_ids.txt')
+            with open(model_ids) as fh:
+                lines = fh.readlines()
+            self.assertEqual(len(lines), 5)
+            for line in lines:
+                num, sample = line.rstrip('\r\n').split()
+                os.unlink(os.path.join(gsm_dir, 'sample_%s' % sample,
+                                       '%s.rmf3' % num))
+            os.unlink(model_ids)
+            os.rmdir(os.path.join(gsm_dir, 'sample_A'))
+            os.rmdir(os.path.join(gsm_dir, 'sample_B'))
+
+            os.rmdir(gsm_dir)
+
 
 if __name__ == '__main__':
     unittest.main()
