eb9c397c2871644ce20ae34e45eb6fa33a15ba5a
shruthivis <shruthi.viswanath@gmail.com>
2020-05-07 17:56:04 +0530

    Fix inconsistent space and tab with Python3

--- pyext/src/Master_Sampling_Exhaustiveness_Analysis.py
+++ pyext/src/Master_Sampling_Exhaustiveness_Analysis.py
@@ -105,9 +105,9 @@ if not args.skip_sampling_precision:
     # afterwards (so that we retain the original IMP orientation)
     numpy.save("conforms", conforms)
     if args.ambiguity:
-	inner_data = get_rmsds_matrix(conforms, args.mode, args.align, args.cores,symm_groups)
+        inner_data = get_rmsds_matrix(conforms, args.mode, args.align, args.cores,symm_groups)
     else:
-	inner_data = get_rmsds_matrix(conforms, args.mode, args.align, args.cores)
+        inner_data = get_rmsds_matrix(conforms, args.mode, args.align, args.cores)
 
     print("Size of RMSD matrix (flattened):",inner_data.shape)
     del conforms
