ea6ab49d73de82851a121e26bb46fdd76047b810
Andrea <graziadei.a@gmail.com>
2021-03-05 17:32:16 +0100

    avoid code duplication in if statement

--- pyext/src/rmsd_calculation.py
+++ pyext/src/rmsd_calculation.py
@@ -15,18 +15,16 @@ import RMF
 def parse_rmsd_selection(h, selection):
     s0 = None
     for idx,selected_range in enumerate(selection.values()):
+        # parse tuple selection in dictionary file for residue ranges
+        s = IMP.atom.Selection(h, resolution=1,
+                               molecule=str(selected_range[0][2]),
+                                            molecule=str(selected_range[0][2]),
+                                            residue_indexes=range(selected_range[0][0],
+                                            selected_range[0][1]))
         if idx == 0:
-            #parse tuple selection in dictionary file for residue ranges
-            s0 = IMP.atom.Selection(h, resolution=1,
-                                    molecule=str(selected_range[0][2]),
-                                    residue_indexes=range(selected_range[0][0],
-                                    selected_range[0][1]))
+            s0 = s
         else:
-            s1 = IMP.atom.Selection(h, resolution=1,
-                                    molecule=str(selected_range[0][2]),
-                                    residue_indexes=range(selected_range[0][0],
-                                    selected_range[0][1]))
-            s0 = s0 | s1
+            s0 |= s
 
     return s0
 
@@ -189,7 +187,6 @@ def parse_symmetric_groups_file(symm_groups_file):
 def get_rmfs_coordinates_one_rmf(path, rmf_A, rmf_B, subunit_name=None,
                                  symm_groups_file=None, selection=None):
     '''Modified RMF coordinates function to work with symmetric copies'''
-
 
     # Open RMFs and get total number of models
     rmf_fh = RMF.open_rmf_file_read_only(os.path.join(path, rmf_A))
