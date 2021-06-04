235b4c47cd258f495e8833909681a95b74b20ff0
Ben Webb <ben@salilab.org>
2021-06-04 13:39:23 -0700

    Revert "Use conda-forge gnuplot"
    This reverts commit 49d1c53aa43d075d8e36aa347fec26ccbfaa6913.
    The conda-forge gnuplot package conflicts with the IMP
    package on Python 2.

--- tools/setup_ci.sh
+++ tools/setup_ci.sh
@@ -10,7 +10,7 @@ fi
 python_version=$1
 
 conda config --remove channels defaults  # get conda-forge, not main, packages
-conda create --yes -q -n python${python_version} -c salilab -c conda-forge python=${python_version} pip scipy matplotlib pandas pyrmsd imp-nightly cmake gnuplot
+conda create --yes -q -n python${python_version} -c salilab -c conda-forge python=${python_version} pip scipy matplotlib pandas pyrmsd imp-nightly cmake
 eval "$(conda shell.bash hook)"
 conda activate python${python_version}
 pip install pytest-cov coverage pytest-flake8
