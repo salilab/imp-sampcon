2206307650599932af657c890bd9b43d9d243a6a
Stochastic13 <28253514+Stochastic13@users.noreply.github.com>
2022-09-10 15:48:12 +0530

    Fix for precision_rmsd

--- pyext/src/precision_rmsd.py
+++ pyext/src/precision_rmsd.py
@@ -52,8 +52,11 @@ def get_particles_from_superposed(
                 calcSymmetryGroups=[],
                 fitSymmetryGroups=symm_groups)
     
-    if calculator_name == 'NOSUP_SERIAL_CALCULATOR':
-        # pairwise returns the superposed calc_coords if specified
+    check1 = (calculator_name == 'NOSUP_SERIAL_CALCULATOR')
+    check2 = not (symm_groups is None)
+    if check1 and check2:
+        # superposed calc_coords returned if calc-coords 
+        # specified while creating the calculator
         rmsd, superposed_fit, _calc_fit = calculator.pairwise(
             0, 1, get_superposed_coordinates=True)
     else:
