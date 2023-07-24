29f252327772d218dcb4b76e0cf522493e2bb24c
Ben Webb <ben@salilab.org>
2023-07-24 16:37:54 -0700

    Improve help text

--- pyext/src/exhaust.py
+++ pyext/src/exhaust.py
@@ -101,10 +101,10 @@ def parse_args():
         action='store_true')
     parser.add_argument(
         '--selection', '-sn', dest="selection",
-        help='file containing dictionary'
-        'of selected subunits and residues'
-        'for RMSD and clustering calculation'
-        "each entry in the dictionary takes the form"
+        help='file containing dictionary '
+        'of selected subunits and residues '
+        'for RMSD and clustering calculation; '
+        "each entry in the dictionary takes the form "
         "'selection name': [(residue_start, residue_end, protein name)",
         default=None)
     parser.add_argument(
